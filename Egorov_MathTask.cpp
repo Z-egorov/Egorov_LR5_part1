e512c0ee6649e1fb00f8f1e6ed60aad68ccd0167
Z-egorov <zheniaegorov1999@gmail.com>
2025-05-23 06:34:56 +0300

    message

--- Egorov_MathTask.cpp
+++ Egorov_MathTask.cpp
@@ -19,17 +19,3 @@ int CalcRectangleArea(int NumberA, int NumberB) {
     return NumberA * NumberB;
 }
 
-int main() {
-    int NumberA = 0, NumberB = 0;
-    
-    EnterDigit(NumberA, "input height A");
-
-    EnterDigit(NumberB, "input height B");
-
-
-    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
-
-    cout << "Area of Rectangle is " << RectangleArea << endl;
-
-    return 0;
-}
